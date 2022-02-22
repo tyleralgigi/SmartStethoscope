fa58c7c1f8352c02b14ccd67fcdbd10cd8b516a5
Tyler <>
2022-02-21 19:50:45 -0500

    Create get_acc_info.js

---
+++ React-Native/src/other/js/get_acc_info.js
@@ -0,0 +1,23 @@
+import { get, getDatabase, ref } from 'firebase/database';
+import { auth } from './firebase';
+
+export const get_acc_info = (user) => {
+    const db = getDatabase();
+    const reference = ref(db, 'users/' + user.uid);
+      get(reference, ).then((snapshot) => {
+        if (snapshot.exists()) {
+          return({
+            email: snapshot.val().email,
+            first_name: snapshot.val().first_name,
+            last_name: snapshot.val().last_name,
+            acc_type: snapshot.val().acc_type,
+            loading: false
+          });
+        } else {
+          console.log("No data available");
+          signOut(auth);
+        }
+      }).catch((error) => {
+        console.error(error);
+      });
+}
